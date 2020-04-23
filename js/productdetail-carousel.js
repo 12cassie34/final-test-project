446a2a3b926b08f3220abe398d6f665ff4910ad1
Ines Lin <inesiscoding@gmail.com>
2020-04-23 16:27:42 +0800

    Product detail version 1 completed

--- js/productdetail-carousel.js
+++ js/productdetail-carousel.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
     	arrows: false,
     	fade: true,
     	mobileFirst: true,
+    	respondTo: 'min',
     	asNavFor: '.slider-nav'
     });
+
     $('.slider-nav').slick({
     	slidesToShow: 4,
     	slidesToScroll: 1,
@@ -14,13 +16,48 @@ $(document).ready(function(){
     	asNavFor: '.slider-for',
     	dots: false,
     	infinite: true,
-    	centerMode: false,
     	variableWidth: true,
     	arrows: true,
     	prevArrow: false,
     	focusOnSelect: true,
     	mobileFirst: true,
-    	focusOnSelect: true
+    	focusOnSelect: true,
+    	respondTo:'min',
+    	responsive: [
+    	{
+    		breakpoint: 721,
+    		settings: {
+    			slidesToShow: 1,
+    			slidesToScroll: 1,
+    			infinite: true,
+    			dots: false,
+    	        centerMode: true,
+    			arrows: true,
+    	        prevArrow: false
+    		}
+    	},
+    	{
+    		breakpoint: 541,
+    		settings: {
+    			slidesToShow: 1,
+    			lidesToScroll: 1,
+    			centerMode: true,
+    			arrows: true,
+    	        prevArrow: false
+    		}
+    	},
+    	{
+    		breakpoint: 320,
+    		settings: {
+    			slidesToShow: 1,
+    			slidesToScroll: 1,
+    			dots: false,
+    			arrows: true,
+    	        prevArrow: false,
+    	        centerMode: true
+    		}
+    	}
+    	]
     });
 
     $('.slick-next').text(">");
